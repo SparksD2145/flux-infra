ee92fda89540e5f3c56dad23ef7ad97d1df6313f
Thomas Ibarra <sparks@imake.software>
2021-08-25 10:43:00 -0500

    rook: fixup drive allocation

--- ansible/storage/nuke-ceph.bash
+++ ansible/storage/nuke-ceph.bash
@@ -1,3 +1,4 @@
+rm -rf /var/lib/rook
 ls /dev/mapper/ceph--* | xargs -I% -- fuser --kill %
 ls /dev/mapper/ceph--* | xargs -I% -- dmsetup clear %
 ls /dev/mapper/ceph--* | xargs -I% -- dmsetup remove -f %
