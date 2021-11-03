1ee1d959f0597349b45f6739f278d2972465ac23
Thomas Ibarra <hello@imake.software>
2021-11-03 10:06:13 -0500

    correction to script

--- provision/scripts/copy-cert.sh
+++ provision/scripts/copy-cert.sh
@@ -1,1 +1,1 @@
-kubectl get secret acme-crt-secret-$1 --namespace="cert-manager" -o yaml | sed "s/namespace: cert-manager0/namespace: $2/g" | kubectl create -f -
+kubectl get secret acme-crt-secret-$1 --namespace="cert-manager" -o yaml | sed "s/namespace: cert-manager/namespace: $2/g" | kubectl create -f -
