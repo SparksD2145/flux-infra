7672ab5532f96ac3c17c4a8141f5f37bedbccdf7
Thomas Ibarra <hello@imake.software>
2021-11-16 21:16:54 -0600

    adjust provision script

--- provision/scripts/install-cluster/production.bash
+++ provision/scripts/install-cluster/production.bash
@@ -3,20 +3,4 @@
 ##########
 
 
-# Prepare cert-manager
-kubectl apply -f infrastructure/production/cert-manager/namespace.yaml
-./infrastructure/production/cert-manager/apply-crds.sh
-kubectl apply -f secrets/cloudflare-certmanager.yaml
-
-# Prepare external-dns
-kubectl apply -f secrets/cloudflare-externaldns.yaml
-
-# Prepare Gitlab
-kubectl apply -f apps/production/gitlab/namespace.yaml
-kubectl apply -f secrets/gitlab-initial-root-password.yaml
-
-# Prepare Node Flags
-kubectl label node lambda zwave-controller=true
-kubectl label node lambda zigbee-controller=true
-
 flux bootstrap github --owner SparksD2145 --personal --private --repository flux-infra --branch master --path ./cluster/base
