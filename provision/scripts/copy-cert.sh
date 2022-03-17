aa660dcfe7d75ce4376d323e943c995c6444d915
Thomas Ibarra <hello@imake.software>
2022-03-17 11:25:40 -0500

    provision: update copy cert to update all namespaces

--- provision/scripts/copy-cert.sh
+++ provision/scripts/copy-cert.sh
@@ -1,1 +1,3 @@
-kubectl get secret acme-crt-secret-$1 --namespace="cert-manager" -o yaml | sed "s/namespace: cert-manager/namespace: $2/g" | kubectl create -f -
+for ns in $(kubectl get ns | tail -n+2 | awk '{ print $1 }'); do
+    kubectl get secret acme-crt-secret-$1 --namespace="cert-manager" -o yaml | sed "s/namespace: cert-manager/namespace: $ns/g" | kubectl create -f -
+done
