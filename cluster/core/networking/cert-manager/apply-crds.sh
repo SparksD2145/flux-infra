4d41e1b868e99caad19e816d71683739d7d57c65
Thomas Ibarra <hello@imake.software>
2022-04-09 16:07:07 -0500

    cert-manager: update script

--- cluster/core/networking/cert-manager/apply-crds.sh
+++ cluster/core/networking/cert-manager/apply-crds.sh
@@ -1,1 +1,1 @@
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.7.0/cert-manager.crds.yaml
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.8.0/cert-manager.crds.yaml
