2728c4b3fb9a06406f1e60871995f5d8253da5ae
Thomas Ibarra <hello@imake.software>
2023-06-29 18:29:07 -0500

    guacamole: deploy

--- provision/terraform/opnsense/unbound_aliases_internal.tf
+++ provision/terraform/opnsense/unbound_aliases_internal.tf
@@ -61,6 +61,15 @@ resource "opnsense_unbound_host_alias" "svc-emqx-dashboard-sparks-codes" {
   domain      = "sparks.codes"
 }
 
+resource "opnsense_unbound_host_alias" "svc-guac-sparks-codes" {
+  override = opnsense_unbound_host_override.lb-internal-services.id
+
+  enabled     = true
+  description = "Guacamole remote access"
+  hostname    = "guac"
+  domain      = "sparks.codes"
+}
+
 resource "opnsense_unbound_host_alias" "svc-idrac-foxtrot-console-sparks-codes" {
   override = opnsense_unbound_host_override.lb-internal-services.id
 
