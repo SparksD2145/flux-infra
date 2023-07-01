995be2bc95ecdf75d9042e596b6c3943e24548ae
Thomas Ibarra <hello@imake.software>
2023-07-01 12:00:08 -0500

    provision: update latest snapshot

--- provision/scripts/install-cluster/production.bash
+++ provision/scripts/install-cluster/production.bash
@@ -1,6 +1,5 @@
-##########
-# PRODUCTION CLUSTER INSTALL SCRIPT
-##########
-
+#####################################
+# PRODUCTION CLUSTER INSTALL SCRIPT #
+#####################################
 
 flux bootstrap github --ssh-key-algorithm ed25519 --owner SparksD2145 --personal --private --repository flux-infra --branch master --path ./cluster/base
