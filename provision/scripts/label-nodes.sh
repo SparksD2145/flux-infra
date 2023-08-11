a34e7c8b5dda206d9a417e5fb762c77d842ce017
Thomas Ibarra <hello@imake.software>
2023-08-10 19:07:49 -0500

    provision: adjust label nodes script

--- provision/scripts/label-nodes.sh
+++ provision/scripts/label-nodes.sh
@@ -1,4 +1,4 @@
-#!/usr/sh
+#!/bin/sh
 
 # rpi-poe-1
 kubectl label node rpi4-poe-1 zigbee-controller=true
@@ -7,11 +7,6 @@ kubectl taint node rpi4-poe-1 iot-control=active:NoSchedule
 
 # Tango
 kubectl label node tango node-role.kubernetes.io/gpu=true
-kubectl label node tango node-role.kubernetes.io/storage=true
 
 # Omega
-kubectl label node omega node-role.kubernetes.io/storage=true
 kubectl label node omega services/minecraft=active
-
-# Whiskey
-kubectl label node whiskey node-role.kubernetes.io/storage=true
