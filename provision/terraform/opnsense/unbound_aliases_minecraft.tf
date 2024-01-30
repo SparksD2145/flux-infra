0af64e150598d602dd86f5bccf3fdbc0fc517eec
Thomas Ibarra <hello@imake.software>
2024-01-30 15:20:51 -0600

    minecraft: deploy superdigdug minecraft instances

--- provision/terraform/opnsense/unbound_aliases_minecraft.tf
+++ provision/terraform/opnsense/unbound_aliases_minecraft.tf
@@ -15,3 +15,48 @@ resource "opnsense_unbound_host_alias" "archive-cwh11-mc-sparks-codes" {
   hostname    = "archive-cwh11"
   domain      = "mc.sparks.codes"
 }
+
+resource "opnsense_unbound_host_alias" "superdigdug-mc-sparks-codes" {
+  override = opnsense_unbound_host_override.lb-minecraft.id
+
+  enabled     = true
+  description = "SuperDigDug's Minecraft Server (Overworld)"
+  hostname    = "superdigdug"
+  domain      = "mc.sparks.codes"
+}
+
+resource "opnsense_unbound_host_alias" "superdigdug-sparks-mc-sparks-codes" {
+  override = opnsense_unbound_host_override.lb-minecraft.id
+
+  enabled     = true
+  description = "SuperDigDug's Minecraft Server (Sparks)"
+  hostname    = "superdigdug-sparks"
+  domain      = "mc.sparks.codes"
+}
+
+resource "opnsense_unbound_host_alias" "superdigdug-newlands-mc-sparks-codes" {
+  override = opnsense_unbound_host_override.lb-minecraft.id
+
+  enabled     = true
+  description = "SuperDigDug's Minecraft Server (Newlands)"
+  hostname    = "superdigdug-newlands"
+  domain      = "mc.sparks.codes"
+}
+
+resource "opnsense_unbound_host_alias" "superdigdug-rectify-mc-sparks-codes" {
+  override = opnsense_unbound_host_override.lb-minecraft.id
+
+  enabled     = true
+  description = "SuperDigDug's Minecraft Server (Rectify)"
+  hostname    = "superdigdug-rectify"
+  domain      = "mc.sparks.codes"
+}
+
+resource "opnsense_unbound_host_alias" "superdigdug-wilderness-mc-sparks-codes" {
+  override = opnsense_unbound_host_override.lb-minecraft.id
+
+  enabled     = true
+  description = "SuperDigDug's Minecraft Server (Wildernerss)"
+  hostname    = "superdigdug-wilderness"
+  domain      = "mc.sparks.codes"
+}
