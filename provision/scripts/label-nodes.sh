ff7dc29a6d8046c08cde4b88207a1e5c2d6a08a8
Thomas Ibarra <hello@iwrite.software>
2024-12-14 20:47:35 -0600

    ollama: deploy on kappa

--- provision/scripts/label-nodes.sh
+++ provision/scripts/label-nodes.sh
@@ -7,6 +7,7 @@ kubectl taint node rpi4-poe-1 iot-control=active:NoSchedule
 
 # Tango
 kubectl label node tango node-role.kubernetes.io/gpu=true
+kubectl label node kappa node-role.kubernetes.io/gpu=true
 
 # Omega
 kubectl label node omega services/minecraft=active
--- provision/terraform/opnsense/unbound_overrides_loadbalancers.tf
+++ provision/terraform/opnsense/unbound_overrides_loadbalancers.tf
@@ -115,6 +115,15 @@ resource "opnsense_unbound_host_override" "lb-paperless-dropbox" {
   server   = "10.10.201.13"
 }
 
+resource "opnsense_unbound_host_override" "lb-ollama" {
+  enabled     = true
+  description = "Ollama Endpoint"
+
+  hostname = "ollama"
+  domain   = "sparks.codes"
+  server   = "10.10.201.14"
+}
+
 resource "opnsense_unbound_host_override" "lb-minecraft" {
   enabled     = true
   description = "Minecraft loadbalancer"
