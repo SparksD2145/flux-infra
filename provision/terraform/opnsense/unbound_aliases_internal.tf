01266702fd2d2912ce2572a48ccfaff96e897a49
Thomas Ibarra <hello@iwrite.software>
2024-12-15 21:53:21 -0600

    ollama-open-webui: deploy

--- provision/terraform/opnsense/unbound_aliases_internal.tf
+++ provision/terraform/opnsense/unbound_aliases_internal.tf
@@ -205,12 +205,12 @@ resource "opnsense_unbound_host_alias" "svc-router" {
   domain      = "sparks.codes"
 }
 
-resource "opnsense_unbound_host_alias" "svc-scrutiny" {
+resource "opnsense_unbound_host_alias" "svc-ollama-webui" {
   override = opnsense_unbound_host_override.lb-internal-services.id
 
   enabled     = true
-  description = "Harddrive Health Monitoring"
-  hostname    = "scrutiny"
+  description = "Ollama frontend"
+  hostname    = "ollama-webui"
   domain      = "sparks.codes"
 }
 
