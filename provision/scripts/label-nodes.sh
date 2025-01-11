a82c7b849d306c7d87b1f8addc6e227e505a2048
Thomas Ibarra <sparks@imake.software>
2025-01-10 21:35:24 -0600

    ollama: move to beta

--- provision/scripts/label-nodes.sh
+++ provision/scripts/label-nodes.sh
@@ -7,6 +7,7 @@ kubectl label node rpi4-poe-1 zwave-controller=true
 # Tango
 kubectl label node tango node-role.kubernetes.io/gpu=true
 kubectl label node kappa node-role.kubernetes.io/gpu=true
+kubectl label node beta node-role.kubernetes.io/gpu=true
 
 # Omega
 kubectl label node omega services/minecraft=active
