f6ddad1f54c512226ad410f4c3d449bcd0e90e95
Thomas Ibarra <hello@iwrite.software>
2025-04-18 15:59:39 -0500

    gitlab: add internal ingress

--- provision/terraform/opnsense/unbound_aliases_internal.tf
+++ provision/terraform/opnsense/unbound_aliases_internal.tf
@@ -88,6 +88,15 @@ resource "opnsense_unbound_host_alias" "svc-emqx-dashboard-sparks-codes" {
   domain      = "sparks.codes"
 }
 
+resource "opnsense_unbound_host_alias" "svc-gitlab-iwrite-software" {
+  override = opnsense_unbound_host_override.lb-internal-services.id
+
+  enabled     = true
+  description = "Gitlab"
+  hostname    = "gitlab"
+  domain      = "iwrite.software"
+}
+
 resource "opnsense_unbound_host_alias" "svc-influxdb-sparks-codes" {
   override = opnsense_unbound_host_override.lb-internal-services.id
 
