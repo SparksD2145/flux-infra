7ea8568e35902b1ced0520d9f414b06864bce337
Thomas Ibarra <hello@iwrite.software>
2025-05-02 18:58:29 -0500

    igpu,nfd: deploy

--- provision/terraform/opnsense/unbound_aliases_internal.tf
+++ provision/terraform/opnsense/unbound_aliases_internal.tf
@@ -106,6 +106,15 @@ resource "opnsense_unbound_host_alias" "svc-emqx-dashboard-sparks-codes" {
   domain      = "sparks.codes"
 }
 
+resource "opnsense_unbound_host_alias" "svc-frigate-sparks-codes" {
+  override = opnsense_unbound_host_override.lb-internal-services.id
+
+  enabled     = true
+  description = "Frigate NVR"
+  hostname    = "frigate"
+  domain      = "sparks.codes"
+}
+
 resource "opnsense_unbound_host_alias" "svc-data-sparks-cdes" {
   override = opnsense_unbound_host_override.lb-internal-services.id
 
