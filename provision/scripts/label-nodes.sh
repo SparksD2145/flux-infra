2fd269a1307058400d6d4670aab82f47a34ba4e7
Thomas Ibarra <hello@iwrite.software>
2025-05-02 15:06:34 -0500

    scripts: update labeler

--- provision/scripts/label-nodes.sh
+++ provision/scripts/label-nodes.sh
@@ -6,7 +6,7 @@ kubectl label node rpi4-poe-2 zigbee-controller=true
 
 # Tango
 kubectl label node tango node-role.kubernetes.io/gpu=true
-kubectl label node gpu-node-1 node-role.kubernetes.io/gpu=true
+kubectl label node beta node-role.kubernetes.io/gpu=true
 
 # Omega
 kubectl label node omega services/minecraft=active
