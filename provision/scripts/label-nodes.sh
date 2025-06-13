67d9abcce61b15d56c8d11fda245242843a654d4
Thomas Ibarra <hello@iwrite.software>
2025-06-12 19:23:46 -0500

    minecraft: disable

--- provision/scripts/label-nodes.sh
+++ provision/scripts/label-nodes.sh
@@ -8,5 +8,6 @@ kubectl label node rpi4-poe-2 zigbee-controller=true
 kubectl label node tango node-role.kubernetes.io/gpu=true
 kubectl label node beta node-role.kubernetes.io/gpu=true
 
-# Omega
-kubectl label node omega services/minecraft=active
+# Lambda
+kubectl label node lambda services/gaming=true
+kubectl label node lambda services/minecraft=true
