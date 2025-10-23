d4292a9a8d658ca3467ac610bde2671553a2c00b
Thomas Ibarra <hello@iwrite.software>
2025-10-23 11:51:07 -0500

    ceph: fix nuke script

--- provision/ansible/playbooks/storage/nuke-ceph.bash
+++ provision/ansible/playbooks/storage/nuke-ceph.bash
@@ -1,5 +1,25 @@
-rm -rf /var/lib/rook
-ls /dev/mapper/ceph--* | xargs -I% -- fuser --kill %
-ls /dev/mapper/ceph--* | xargs -I% -- dmsetup clear %
-ls /dev/mapper/ceph--* | xargs -I% -- dmsetup remove -f %
-ls /dev/mapper/ceph--* | xargs -I% -- rm -rf %
+DISK="$1"
+
+# Zap the disk to a fresh, usable state (zap-all is important, b/c MBR has to be clean)
+sgdisk --zap-all $DISK
+wipefs -af $DISK
+
+# Wipe portions of the disk to remove more LVM metadata that may be present
+dd if=/dev/zero of="$DISK" bs=1K count=200 oflag=direct,dsync seek=0 # Clear at offset 0
+dd if=/dev/zero of="$DISK" bs=1K count=200 oflag=direct,dsync seek=$((1 * 1024**2)) # Clear at offset 1GB
+dd if=/dev/zero of="$DISK" bs=1K count=200 oflag=direct,dsync seek=$((10 * 1024**2)) # Clear at offset 10GB
+dd if=/dev/zero of="$DISK" bs=1K count=200 oflag=direct,dsync seek=$((100 * 1024**2)) # Clear at offset 100GB
+dd if=/dev/zero of="$DISK" bs=1K count=200 oflag=direct,dsync seek=$((1000 * 1024**2)) # Clear at offset 1000GB
+
+# SSDs may be better cleaned with blkdiscard instead of dd
+blkdiscard $DISK
+
+# Inform the OS of partition table changes
+partprobe $DISK
+
+# This command hangs on some systems: with caution, 'dmsetup remove_all --force' can be used
+ls /dev/mapper/ceph-* | xargs -I% -- dmsetup remove %
+
+# ceph-volume setup can leave ceph-<UUID> directories in /dev and /dev/mapper (unnecessary clutter)
+rm -rf /dev/ceph-*
+rm -rf /dev/mapper/ceph--*
