160cacb6cfa9bc5abcf5793589fbde19e6c9db7c
Thomas Ibarra <hello@iwrite.software>
2026-02-03 19:10:00 -0600

    traefik: deploy replacing nginx ingress controller

--- provision/terraform/opnsense/unbound_overrides_global.tf
+++ provision/terraform/opnsense/unbound_overrides_global.tf
@@ -1,8 +1,8 @@
-resource "opnsense_unbound_host_override" "nexus-switch" {
-  enabled     = true
-  description = "The Cisco Nexus 5548UP Network switch"
+# resource "opnsense_unbound_host_override" "nexus-switch" {
+#   enabled     = true
+#   description = "The Cisco Nexus 5548UP Network switch"
 
-  hostname = "nexus"
-  domain   = "sparks.codes"
-  server   = "10.10.0.5"
-}
+#   hostname = "nexus"
+#   domain   = "sparks.codes"
+#   server   = "10.10.0.5"
+# }
