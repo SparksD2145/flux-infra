e4cde5822f2bf0383d6fd43a55bb9d5df907ec6c
Thomas Ibarra <hello@iwrite.software>
2026-03-08 18:41:03 -0500

    metube: adjust media folder

--- provision/scripts/label-nodes.sh
+++ provision/scripts/label-nodes.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 # rpi settings
-kubectl taint node rpi4-poe-1 low-memory=true:NoSchedule
-kubectl taint node rpi4-poe-2 low-memory=true:NoSchedule
-kubectl taint node rpi4-poe-3 low-memory=true:NoSchedule
-kubectl label node rpi4-poe-1 zwave-controller=true
-kubectl label node rpi4-poe-2 zigbee-controller=true
+# kubectl taint node rpi4-poe-1 low-memory=true:NoSchedule
+# kubectl taint node rpi4-poe-2 low-memory=true:NoSchedule
+# kubectl taint node rpi4-poe-3 low-memory=true:NoSchedule
+# kubectl label node rpi4-poe-1 zwave-controller=true
+# kubectl label node rpi4-poe-2 zigbee-controller=true
 
 # Tango
 kubectl label node tango node-role.kubernetes.io/gpu=true
 
 # VMs
-kubectl label node vm-gpu-1 node-role.kubernetes.io/gpu=true
+# kubectl label node vm-gpu-1 node-role.kubernetes.io/gpu=true
 
 # Lambda
 kubectl label node lambda services/gaming=true
